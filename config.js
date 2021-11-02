db1823c66fd000f7a23dfa6e23a70f53d2d8ab52
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-02 11:42:52 +0700

    Create config.js

--- config.js
+++ config.js
@@ -2,28 +2,28 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '6281390658325': { // put your number here
+  '6283102050562': { // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
+  '6283102050562': { // put your number here
     name: 'Dave',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '60199782326': {
+  '6283101050562': {
     name: 'Adiixyz',
     isReport: true, // if true this number will send if user use command /report or /join
   },
-  '6285713964963': {
+  '6283102050562': {
     isReport: true
   },
-  '13479805233': {} // ignore if you don't want anything, like name dll
+  '6283102050562': {} // ignore if you don't want anything, like name dll
 }
-global.mods = ['13479805233'] // Want some help?
+global.mods = ['6283102050562'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -46,7 +46,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
+global.packname = '🤗'
 global.author = 'Metro Bot Is The Best'
 
 global.multiplier = 39 // The higher, The harder levelup
