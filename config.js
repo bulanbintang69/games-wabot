fd0459135f1d8b55d408e529dc0860ef9e4d5735
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-13 02:18:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.xteam.xyz': '8494d81ab84c9d20',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais'
